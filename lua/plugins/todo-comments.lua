2db4f07f786e51a71fb4e2188d75bc28286c0a1d
Abishevs <eduards@abishevs.dev>
2025-03-23 20:04:30 +0100

    feat: Ayoo Todo.nvim is amazing plugin!

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+-- TODO: Make A bind for telescope view
+-- TODO: Explore other possibilities
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+  },
+}
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,1 +1,26 @@
-return {}
+return {
+
+    'mbbill/undotree',
+    config = function()
+        -- Set the undo directory
+        local target_path = vim.fn.expand("~/.undodir")
+        
+        -- Create the directory if it doesn't exist
+        if vim.fn.isdirectory(target_path) == 0 then
+            vim.fn.mkdir(target_path, "p")
+        end
+
+        -- Configure undo settings
+        vim.o.undodir = target_path
+        vim.o.undofile = true
+
+        -- Keybinding to toggle the UndoTree
+	vim.keymap.set('n', '<leader>uu', function()
+		vim.cmd.UndotreeToggle() 
+		vim.cmd.UndotreeFocus() 
+	end)
+
+
+    end
+}
+
